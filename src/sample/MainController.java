531eb615619b10a0752c1a0023b13d9c52594770
s20501 <56770915+s20501@users.noreply.github.com>
2020-06-01 21:08:10 +0200

    Bazowy controller

---
+++ src/sample/MainController.java
@@ -0,0 +1,32 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+public class MainController {
+
+    @FXML
+    private Button histogramBtn ;
+
+    @FXML
+    private Button importBtn ;
+
+    @FXML
+    private TextArea textForm ;
+
+    public void initialize() {
+        // initialization here, if needed...
+    }
+
+    @FXML
+    private void createHistogram(ActionEvent event) {
+            System.out.println("Histogram click");
+    }
+
+    @FXML
+    private void importFile(ActionEvent event) {
+        System.out.println("Import Click");
+    }
+}
