844c2130c6df2d48c8671dc5018c0e90e79f90bd
s20501 <56770915+s20501@users.noreply.github.com>
2020-06-05 20:53:42 +0200

    Zapisywanie do pliku

--- src/histogram/Main.java
+++ src/histogram/Main.java
@@ -9,6 +9,7 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    // Start aplikacji
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("View/main.fxml"));
