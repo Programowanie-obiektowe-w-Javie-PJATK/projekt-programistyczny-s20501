1436d7a15d9cbceefc3019735fc2537df1f52c52
s20501 <56770915+s20501@users.noreply.github.com>
2020-06-05 17:20:17 +0200

    Poprawki do wyglądu

--- src/histogram/Controller/PreviewController.java
+++ src/histogram/Controller/PreviewController.java
@@ -1,7 +1,6 @@
 package histogram.Controller;
 
 import histogram.Class.HistogramText;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.text.Text;
 
@@ -22,8 +21,5 @@ public class PreviewController {
         textForm.setText(this.histogram.getHistogram());
     }
 
-    public void sort(ActionEvent actionEvent) {
-        this.histogram.sort();
 
-    }
 }
